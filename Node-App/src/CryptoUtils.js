9f8a9ab1446b60de4e1f5fdbd819eea4a74b4e83
Sharath <sharatkm@gmail.com>
2019-11-12 17:10:34 -0600

    public, private and signature utils

--- Node-App/src/CryptoUtils.js
+++ Node-App/src/CryptoUtils.js
@@ -3,6 +3,70 @@ const ec = new EC('secp256k1');
 
 var CryptoJS = require('crypto-js');
 
+// Generate random private key using elliptic curve
+function generateRandomPrivateKey() {
+    let privateKey = ec.genKeyPair().getPrivate('hex');
+    return privateKey;
+}
+
+// Get the public key from private key
+// Get the Public/Private key pair from the Private Key String. Then, you get the Public Key (x, y) coordinate from the Key Pair object.
+// Take the "x" coordinate value of the Public Key and concatenate it at the end with either a "0" or "1" depending upon whether the "y" coordinate value is even or odd.
+// Input: A 64-Hex Digit string representing a Private Key
+// Output: A 65-Hex Digit string representing the Compressed version of the derived Public Key
+function getPublicKeyFromPrivateKey(privateKeyString) {
+
+    let keyPair = ec.keyFromPrivate(privateKeyString);
+
+    // Below is the Public Key with it's X and Y co-ordinate.
+    let keyPairPublic = keyPair.getPublic();
+
+    // Get the below X and Y co-ordinate values for the Public Key.
+    // They will be in JavaScript BN (BigNumber) object type.
+    let publicKey_X = keyPairPublic.getX();
+    let publicKey_Y = keyPairPublic.getY();
+
+    // Get the Public Key X coordinate in Hexadecimal String format.
+    let publicKey_X_hex_String = publicKey_X.toString(16);
+
+    // Sometimes the Hex string returned is LESS than the 64-Hex string that is expected. So, we need to pad with '0'.
+    publicKey_X_hex_String = publicKey_X_hex_String.padStart(64, '0');
+
+    // Get the Public Key Y coordinate in Binary String format. It will be used later
+    // to determine if a 1 or 0 should be concatenated to the "x" co-ordinate.
+    let publicKey_Y_binary_String = publicKey_Y.toString(2);
+
+    // If the last binary digit is "0", then it's even. If the last binary digit is "1", then it's odd.
+    // A publicKey_Y_binary_String[publicKey_Y_binary_String.length - 1] value of 0 means it's EVEN.
+    // A publicKey_Y_binary_String[publicKey_Y_binary_String.length - 1] value of 1 means it's ODD.
+    let returned_publicKeyString = publicKey_X_hex_String + publicKey_Y_binary_String[publicKey_Y_binary_String.length - 1];
+    return returned_publicKeyString;
+
+}
+
+// Creates a Signature from the input "message" string and "privateKey" 64-hex string.
+// Input:
+// 1) message: any string value
+// 2) privateKey: privateKey of a Public Address that is in 64-hex string format
+//
+// Output: A Signature JavaScript object that has the following two attributes:
+// 1) r : 64-Hex string of the Signature "r" attribute
+// 2) s : 64-Hex string of the Signature "s" attribute
+function createSignature(message, privateKey) {
+    let privateKeyPair = ec.keyFromPrivate(privateKey);
+    let signature = ec.sign(message, privateKeyPair);
+
+    // Sometimes the Hex string returned is LESS than the 64-Hex string that is expected. So, we need to pad with '0'.
+    let signature_r_string = signature.r.toString(16).padStart(64, '0');
+    let signature_s_string = signature.s.toString(16).padStart(64, '0');
+
+    let signatureObject = {r: signature_r_string, s: signature_s_string};
+    return signatureObject;
+}
+
+
+
+// Get Public address from Public Key by taking the RIPEMD-160 of Public Key.
 function getPublicAddressFromPublicKey(publicKeyString) {
     let publicAddress = CryptoJS.RIPEMD160(publicKeyString);
     let publicAddressStr = publicAddress.toString();
@@ -38,7 +102,11 @@ function verifySignature(message, compressedPublicKey, signature) {
 }
 
 module.exports = {
+    generateRandomPrivateKey,
+    getPublicKeyFromPrivateKey,
+    getPublicAddressFromPublicKey,
     uncompressPublicKey,
+    createSignature,
     verifySignature,
-    getPublicAddressFromPublicKey
+
 };
